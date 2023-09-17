126c7e87360a8f817529638834aa182a16672cdc
saransh-283 <saranshmm@gmail.com>
2023-09-18 00:02:47 +0530

    World data structures

---
+++ Assets/Scripts/Connection.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Connection
+{
+    public string connectionName;
+    public string description;
+    public Location location;
+    public bool connectionEnabled;
+}
---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location : MonoBehaviour
+{
+    public string locationName;
+
+    [TextArea]
+    public string description;
+
+    public Connection[] connections;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
